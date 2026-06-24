5ddf4f8302036db28b4bff427f6d34f764f9d44e
Bruno Bortolon da Silva <bbortolondasilva@gmail.com>
2026-06-24 17:47:28 -0300

    Update

--- pages/downloads/script.js
+++ pages/downloads/script.js
@@ -1,12 +1,15 @@
+console.log("Script Carregado!")
+
 function baixar() {
+    console.log(window.location.search)
     const params = new URLSearchParams(window.location.search)
     const arquivo = params.get("arquivo")
 
     const caminho = "../../assets/download/" + arquivo
 
-    const botao = document.getElementById("baixar-novamente")
-    botao.href = caminho
-    botao.download = arquivo
+    const b_novamente = document.getElementById("baixar-novamente")
+    b_novamente.href = caminho
+    b_novamente.download = arquivo
 
     const link = document.createElement("a")
     link.href = caminho
